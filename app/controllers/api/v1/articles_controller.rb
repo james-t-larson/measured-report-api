f3211b20e2fbdc9bc7a44c3466d5eced143dcbb0
James Larson <timothy_james@tuta.io>
2025-05-04 10:09:11 -0700

    fix(articles): limit article index to 10 newest records and fix incorrect category usage

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -8,10 +8,12 @@ class Api::V1::ArticlesController < Api::BaseController
   def index
     @articles = Article.all
     if params[:category_id].nil?
+      @articles = Article.order(created_at: :desc).limit(10)
       generic_render(data: @articles)
     else
       @category = Category.find(params[:category_id])
-      generic_render(data: @category.articles, category: @category.name)
+      @articles = @category.articles.order(created_at: :desc).limit(10)
+      generic_render(data: @articles, category: @category.name)
     end
   end
 
